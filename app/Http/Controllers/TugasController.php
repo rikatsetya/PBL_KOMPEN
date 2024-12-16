9009783f22756103f2b2f8e0536b4b1812db9df6
Rafli Rasyiq <gaplekrasyiq@gmail.com>
2024-12-16 15:44:15 +0700

    update penambahan function pilih pada TugasController untuk mobile

--- app/Http/Controllers/TugasController.php
+++ app/Http/Controllers/TugasController.php
@@ -2,84 +2,246 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\JenisModel;
+use App\Models\LevelModel;
 use App\Models\TugasModel;
+use App\Models\UserModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Yajra\DataTables\DataTables;
 
 class TugasController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
+    // public function index()
+    // {
+    //     $data = TugasModel::select(
+    //         'm_user.user_id',
+
+    //         'nama',
+    //         't_tugas.tugas_id',
+    //         't_tugas.user_id',
+    //         't_tugas.tugas_nama',
+    //         't_tugas.deskripsi',
+    //         't_tugas.tugas_bobot',
+    //         't_tugas.tugas_tenggat',
+    //         't_tugas.periode',
+    //     )
+    //         ->rightJoin('m_user', 't_tugas.user_id', '=', 'm_user.user_id')
+    //         ->get();
+
+    //     return response()->json($data);
+    // }
+
+
+    // /**
+    //  * Show the form for creating a new resource.
+    //  */
+    // public function create()
+    // {
+    //     //
+    // }
+
+    // /**
+    //  * Store a newly created resource in storage.
+    //  */
+    // public function store(Request $request)
+    // {
+    //     // Validasi input
+    //     $validatedData = $request->validate([
+    //         'user_id' => 'required|integer|exists:m_user,user_id',
+    //         'tugas_nama' => 'required|string|max:100',
+    //         'deskripsi' => 'required|string',
+    //         'tugas_bobot' => 'required|integer',
+    //         'tugas_tenggat' => 'required|date',
+    //         'periode' => 'required|string',
+    //     ]);
+
+    //     try {
+    //         // Buat data baru
+    //         $tugas = TugasModel::create([
+    //             'user_id' => $validatedData['user_id'],
+    //             'tugas_nama' => $validatedData['tugas_nama'],
+    //             'deskripsi' => $validatedData['deskripsi'], // Jika null, berikan nilai kosong
+    //             'tugas_bobot' => $validatedData['tugas_bobot'],
+    //             'tugas_tenggat' => $validatedData['tugas_tenggat'],
+    //             'periode' => $validatedData['periode'],
+    //         ]);
+
+    //         // Berikan respon sukses
+    //         return response()->json([
+    //             'message' => 'Tugas berhasil ditambahkan',
+    //             'data' => $tugas,
+    //         ], 201);
+    //     } catch (\Exception $e) {
+    //         // Tangani error jika ada kesalahan
+    //         return response()->json([
+    //             'message' => 'Terjadi kesalahan saat menambahkan tugas',
+    //             'error' => $e->getMessage(),
+    //         ], 500);
+    //     }
+    //     // $data = TugasModel::all();
+    //     // $data->create($request->all());
+    //     // return $data;
+    // }
+
+    // /**
+    //  * Display the specified resource.
+    //  */
+    // public function show(string $id)
+    // {
+    //     //
+    // }
+
+    // /**
+    //  * Show the form for editing the specified resource.
+    //  */
+    // public function edit(string $id)
+    // {
+    //     //
+    // }
+
+    // /**
+    //  * Update the specified resource in storage.
+    //  */
+    // public function update(Request $request, string $id)
+    // {
+    //     //
+    // }
+
+    // /**
+    //  * Remove the specified resource from storage.
+    //  */
+    // public function destroy(string $id)
+    // {
+    //     //
+    // }
+
+    
+    /**
+     * Display a listing of the resource.
+     */
     public function index()
     {
-        $data = TugasModel::select(
-            'm_user.user_id',
-
-            'nama',
-            't_tugas.tugas_id',
-            't_tugas.user_id',
-            't_tugas.tugas_nama',
-            't_tugas.deskripsi',
-            't_tugas.tugas_bobot',
-            't_tugas.tugas_tenggat',
-            't_tugas.periode',
-        )
-            ->rightJoin('m_user', 't_tugas.user_id', '=', 'm_user.user_id')
-            ->get();
-
-        return response()->json($data);
+        $breadcrumb = (object) [
+            'title' => 'Daftar Tugas',
+            'list' => ['Home', 'Tugas']
+        ];
+        $page = (object) [
+            'title' => 'Daftar Tugas yang terdaftar dalam sistem'
+        ];
+        $activeMenu = 'tugas';
+        $activeSubMenu = '';
+        $user = UserModel::all();
+        return view('tugas.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu,'activeSubMenu' => $activeSubMenu, 'user' => $user]);
     }
 
+    public function indexTugas()
+    {
+        $breadcrumb = (object) [
+            'title' => 'Daftar Tugas',
+            'list' => ['Home', 'Tugas']
+        ];
+        $page = (object) [
+            'title' => 'Daftar Tugas yang terdaftar dalam sistem'
+        ];
+        $activeMenu = 'daftar_tugas';
+        $activeSubMenu = '';
+        $user = UserModel::all();
+        return view('daftar_tugas.index', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu, 'activeSubMenu' => $activeSubMenu, 'user' => $user]);
+    }
+
+    public function list(Request $request)
+    {
+        $tugas = TugasModel::select('tugas_id','jenis_id', 'm_user.user_id','m_user.level_id', 'tugas_nama', 'deskripsi', 'tugas_bobot', 'tugas_tenggat', 'periode')->join('m_user', 't_tugas.user_id', '=','m_user.user_id')->with('jenis')->with('user');
+
+        if ($request->level_id) {
+            $tugas->where('level_id', $request->level_id);
+        }
+        return DataTables::of($tugas)
+            ->addIndexColumn() // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex) 
+            ->addColumn('aksi', function ($tugas) {
+                $btn  = '<button onclick="modalAction(\'' . url('/tugas/' . $tugas->tugas_id . '/show_ajax') . '\')" class="btn btn-info btn-sm" title="Detail tugas">Detail</button>';
+                $btn .= ' <button onclick="modalAction(\'' . url('/tugas/' . $tugas->tugas_id . '/edit_ajax') . '\')" class="btn btn-warning btn-sm" title="Edit tugas">Edit</button>';
+                $btn .= ' <button onclick="modalAction(\'' . url('/tugas/' . $tugas->tugas_id . '/delete_ajax') . '\')" class="btn btn-danger btn-sm" title="Hapus tugas">Hapus</button>';
+                return $btn;
+            })
+            ->rawColumns(['aksi']) // memberitahu bahwa kolom aksi adalah html 
+            ->make(true);
+    }
+
+    public function listTugas(Request $request)
+    {
+        $tugas = TugasModel::select('tugas_id','jenis_id', 'm_user.user_id','m_user.level_id', 'tugas_nama', 'deskripsi', 'tugas_bobot', 'tugas_tenggat', 'periode')->join('m_user', 't_tugas.user_id', '=','m_user.user_id')->with('jenis')->with('user');
+
+        if ($request->level_id) {
+            $tugas->where('level_id', $request->level_id);
+        }
+        return DataTables::of($tugas)
+            ->addIndexColumn() // menambahkan kolom index / no urut (default nama kolom: DT_RowIndex) 
+            ->addColumn('aksi', function ($tugas) {
+                $btn = '<button onclick="modalAction(\'' . url('/daftar_tugas/' . $tugas->tugas_id) . '\')" class="btn btn-info btn-sm">detail</button> ';
+                return $btn;
+            })
+            ->rawColumns(['aksi']) // memberitahu bahwa kolom aksi adalah html 
+            ->make(true);
+    }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create_ajax()
     {
-        //
+        $jenis = JenisModel::select('jenis_id', 'jenis_nama')->get();
+        $user = UserModel::select('user_id', 'username')->get();
+
+        return view('tugas.create_ajax')
+            ->with('jenis', $jenis)
+            ->with('user', $user);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store_ajax(Request $request)
     {
-        // Validasi input
-        $validatedData = $request->validate([
-            'user_id' => 'required|integer|exists:m_user,user_id',
-            'tugas_nama' => 'required|string|max:100',
-            'deskripsi' => 'required|string',
-            'tugas_bobot' => 'required|integer',
-            'tugas_tenggat' => 'required|date',
-            'periode' => 'required|string',
-        ]);
+        if ($request->ajax() || $request->wantsJson()) {
+            // Aturan validasi
+            $rules = [
+                'jenis_id' => 'required|integer',
+                'tugas_nama' => 'required|string|max:255',
+                'deskripsi' => 'required|min:1',
+                'tugas_bobot' => 'required|integer',
+                'tugas_tenggat' => 'required|string',
+                'periode' => 'required|string',
+            ];
 
-        try {
-            // Buat data baru
-            $tugas = TugasModel::create([
-                'user_id' => $validatedData['user_id'],
-                'tugas_nama' => $validatedData['tugas_nama'],
-                'deskripsi' => $validatedData['deskripsi'], // Jika null, berikan nilai kosong
-                'tugas_bobot' => $validatedData['tugas_bobot'],
-                'tugas_tenggat' => $validatedData['tugas_tenggat'],
-                'periode' => $validatedData['periode'],
+            // Menggunakan Validator untuk memvalidasi input
+            $validator = Validator::make($request->all(), $rules);
+
+            // Jika validasi gagal, kembalikan respon JSON dengan pesan error
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Validasi Gagal',
+                    'msgField' => $validator->errors() // pesan error validasi
+                ]);
+            }
+            $request['user_id'] = 1;
+            // Jika validasi berhasil, simpan data user
+            TugasModel::create($request -> all());
+
+            // Kembalikan respon JSON berhasil
+            return response()->json([
+                'status' => true,
+                'message' => 'Data tugas berhasil disimpan',
             ]);
-
-            // Berikan respon sukses
-            return response()->json([
-                'message' => 'Tugas berhasil ditambahkan',
-                'data' => $tugas,
-            ], 201);
-        } catch (\Exception $e) {
-            // Tangani error jika ada kesalahan
-            return response()->json([
-                'message' => 'Terjadi kesalahan saat menambahkan tugas',
-                'error' => $e->getMessage(),
-            ], 500);
         }
-        // $data = TugasModel::all();
-        // $data->create($request->all());
-        // return $data;
+
+        // Jika bukan request Ajax, redirect ke halaman utama
+        return redirect('/');
     }
 
     /**
@@ -87,30 +249,113 @@ class TugasController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $daftar_tugas = TugasModel::select('tugas_id','jenis_id', 'm_user.user_id','m_user.level_id', 'tugas_nama', 'deskripsi', 'tugas_bobot', 'tugas_tenggat', 'periode')->join('m_user', 't_tugas.user_id', '=','m_user.user_id')->with('jenis')->with('user')->find($id);
+        $breadcrumb = (object) ['title' => 'Daftar Tugas', 'list' => ['Home', 'Daftar Tugas', 'Detail']];
+        $page = (object) ['title' => 'Daftar Tugas'];
+        $activeMenu = 'daftar_tugas';
+        $activeSubMenu = '';
+        return view('daftar_tugas.show', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu, 'activeSubMenu' => $activeSubMenu, 'daftar_tugas' => $daftar_tugas]);
+    }
+    public function show_ajax(string $id)
+    {
+        $tugas = TugasModel::select('tugas_id','jenis_id', 'm_user.user_id','m_user.level_id', 'tugas_nama', 'deskripsi', 'tugas_bobot', 'tugas_tenggat', 'periode')->join('m_user', 't_tugas.user_id', '=','m_user.user_id')->with('jenis')->with('user')->find($id);
+        $breadcrumb = (object) ['title' => 'Daftar Tugas', 'list' => ['Home', 'Daftar Tugas', 'Detail']];
+        $page = (object) ['title' => 'Daftar Tugas'];
+        $activeMenu = 'tugas';
+        $activeSubMenu = '';
+        return view('tugas.show_ajax', ['breadcrumb' => $breadcrumb, 'page' => $page, 'activeMenu' => $activeMenu, 'activeSubMenu' => $activeSubMenu, 'tugas' => $tugas]);
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit_ajax(string $id)
     {
-        //
+        $tugas = TugasModel::findOrFail($id);
+
+        $jenis = JenisModel::select('jenis_id', 'jenis_nama')->get();
+        $user = UserModel::select('user_id', 'username')->get();
+
+        // Return the edit view with user and levels data
+        return view('tugas.edit_ajax', [
+            'tugas' => $tugas,
+            'jenis' => $jenis,
+            'user' => $user
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update_ajax(Request $request, string $id)
     {
-        //
+        if ($request->ajax() || $request->wantsJson()) {
+            $rules = [
+                'user_id' => 'nullable|integer',
+                'jenis_id' => 'required|integer',
+                'tugas_nama' => 'required|string|max:255',
+                'deskripsi' => 'required|min:1',
+                'tugas_bobot' => 'required|integer',
+                'tugas_tenggat' => 'required|string',
+                'periode' => 'required|string',
+            ];
+            // use Illuminate\Support\Facades\Validator;
+            $validator = Validator::make($request->all(), $rules);
+
+            if ($validator->fails()) {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Validasi gagal!',
+                    'msgField' => $validator->errors()
+                ]);
+            }
+
+            $check = TugasModel::find($id);
+            if ($check) {
+                $check->update($request->all());
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Data berhasil diperbarui!'
+                ]);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Data tidak ditemukan!'
+                ]);
+            }
+        }
+
+        return redirect('/');
+    }
+
+    public function confirm_ajax(string $id)
+    {
+        $tugas = TugasModel::find($id);
+        return view('tugas.confirm_ajax', ['tugas' => $tugas]);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function delete( Request $request,string $id)
     {
-        //
+        if ($request->ajax() || $request->wantsJson()) {
+            $tugas = TugasModel::find($id);
+            if ($tugas) {
+                $tugas->delete();
+                return response()->json([
+                    'status' => true,
+                    'message' => 'Data berhasil dihapus!'
+                ]);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Data tidak ditemukan!'
+                ]);
+            }
+        }
+
+        return redirect('/');
     }
 }
+
--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\api\TugasController;
 use App\Http\Controllers\MahasiswaKompenControllerMbl;
 use App\Http\Controllers\TugasController as ControllersTugasController;
 use App\Http\Controllers\TugasControllerMbl;
+use App\Http\Controllers\AbsensiController;
+use App\Http\Controllers\api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -20,12 +22,64 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Route::post('all_data', [TugasControllerMbl::class, 'index']);
+// Route::post('input_data', [TugasControllerMbl::class, 'store']);
+    
+// Route::post('list_mhs_kompen', [MahasiswaKompenControllerMbl::class, 'index']);
+// Route::post('show_list', [MahasiswaKompenControllerMbl::class, 'show']);
+
+Route::post('/login', App\Http\Controllers\Api\LoginController::class)->name('login');
+Route::post('/register', App\Http\Controllers\Api\RegisterController::class)->name('register');
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/logout', App\Http\Controllers\Api\LoginController::class)->name('logout');
+Route::middleware('auth:api')->group(function () {
 
-Route::post('all_data', [TugasControllerMbl::class, 'index']);
-Route::post('input_data', [TugasControllerMbl::class, 'store']);
+    Route::group(['prefix' => 'tugas'], function () {
+        Route::post('jenis_periode_data', [App\Http\Controllers\api\TugasController::class, 'tugasJenisPeriode']);
+        Route::post('tambah_tugas', [App\Http\Controllers\api\TugasController::class, 'create']);
+        Route::post('tugas_data', [App\Http\Controllers\api\TugasController::class, 'showForDT']);
+        Route::post('all_tugas_data', [App\Http\Controllers\api\TugasController::class, 'showForMhs']);
+        Route::post('pilih_tugas', [App\Http\Controllers\api\TugasController::class, 'pilih']);
+    });
+    Route::group(['prefix' => 'alpha'], function () {
+        Route::post('all_data', [App\Http\Controllers\api\AbsensiController::class, 'index']);
+        Route::post('show_data', [App\Http\Controllers\api\AbsensiController::class, 'show']);
+    });
     
-Route::post('list_mhs_kompen', [MahasiswaKompenControllerMbl::class, 'index']);
-Route::post('show_list', [MahasiswaKompenControllerMbl::class, 'show']);
+    Route::group(['prefix' => 'kompen'], function () {
+        Route::post('list_mhs_kompen', [App\Http\Controllers\api\MahasiswaKompenController::class, 'index']);
+        Route::post('show_list', [App\Http\Controllers\api\MahasiswaKompenController::class, 'show']);
+    });
+    
+    Route::group(['prefix' => 'mahasiswa'], function () {
+        Route::post('mahasiswa_data', [\App\Http\Controllers\api\MahasiswaController::class, 'show']);
+        Route::post('edit_profile', [\App\Http\Controllers\api\MahasiswaController::class, 'edit']);
+        Route::post('edit_password', [\App\Http\Controllers\api\MahasiswaController::class, 'update']);
+        Route::post('show_kompen_selesai', [\App\Http\Controllers\api\KompenSelesaiMhsController::class, 'showAllData']);
+    });
+    
+    Route::group(['prefix' => 'dosen'], function () {
+        Route::post('dosen_data', [\App\Http\Controllers\api\DosenController::class, 'show']);
+        Route::post('edit_profile', [\App\Http\Controllers\api\DosenController::class, 'edit']);
+        Route::post('edit_password', [\App\Http\Controllers\api\DosenController::class, 'update']);
+        Route::post('show_kompen_selesai', [\App\Http\Controllers\api\KompenSelesaiAController::class, 'showAllData']);
+        Route::post('detail_kompen_selesai', [\App\Http\Controllers\api\KompenSelesaiAController::class, 'showTaskDetail']);
+        Route::post('update_kompen_selesai', [\App\Http\Controllers\api\KompenSelesaiAController::class, 'updateStatusAndReason']);
+    });
+
+    Route::group(['prefix' => 'tendik'], function () {
+        Route::post('tendik_data', [\App\Http\Controllers\api\TendikController::class, 'show']);
+        Route::post('edit_profile', [\App\Http\Controllers\api\TendikController::class, 'edit']);
+        Route::post('edit_password', [\App\Http\Controllers\api\TendikController::class, 'update']);
+        Route::post('show_kompen_selesai', [\App\Http\Controllers\api\KompenSelesaiAController::class, 'showAllData']);
+        Route::post('detail_kompen_selesai', [\App\Http\Controllers\api\KompenSelesaiAController::class, 'showTaskDetail']);
+        Route::post('update_kompen_selesai', [\App\Http\Controllers\api\KompenSelesaiAController::class, 'updateStatusAndReason']);
+    });
+});
+
